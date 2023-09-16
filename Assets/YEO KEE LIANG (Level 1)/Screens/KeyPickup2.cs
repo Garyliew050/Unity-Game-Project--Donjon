6e1caf14f6f76d34be760b010f2c07febe34ad6b
Keeliang <keeliang13@gmail.com>
2023-09-16 21:59:26 +0800

    puzzles 100%
    puzzles 100%

--- Assets/YEO KEE LIANG (Level 1)/Screens/KeyPickup2.cs
+++ Assets/YEO KEE LIANG (Level 1)/Screens/KeyPickup2.cs
@@ -1,46 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 
-public class AN_DoorKey : MonoBehaviour
+public class KeyPickup2 : MonoBehaviour
 {
-    [Tooltip("True - red key object, false - blue key")]
-    public bool isRedKey = true;
+    public GameObject trapsToDestroy;
 
     [Tooltip("Distance at which the object is considered 'Near'")]
-    public float nearDistance = 50f;
+    public float nearDistance = 20f;
     public TextMeshProUGUI keyCountText;
 
     AN_HeroInteractive hero;
-    
+
 
     private void Start()
     {
         hero = FindObjectOfType<AN_HeroInteractive>(); // key will get up and it will saved in "inventory"
     }
 
+
     void Update()
     {
         if (NearView() && Input.GetKeyDown(KeyCode.E))
         {
-            if (isRedKey)
-            {
-                hero.RedKeyCount++;
-                UpdateKeyCounterUI();
-            }
-
-            else
-            {
-                hero.BlueKey = true;
-            }
-            Destroy(gameObject);
-            SoundManagerScript.PlaySound("takeKey");
+            DestroyTraps();
+            hero.RedKeyCount++;
+            UpdateKeyCounterUI();
+            Destroy(gameObject); // Remove the key GameObject.
             SoundManagerScript.PlaySound("win");
+            SoundManagerScript.PlaySound("takeKey");
+
+        }
+      
+
+    }
+    private void DestroyTraps()
+    {
+        if (trapsToDestroy != null)
+        {
+            Destroy(trapsToDestroy); // Destroy the wall GameObject.
+        }
+        else
+        {
+            Debug.LogError("traps GameObject reference is missing.");
         }
     }
-
     bool NearView() // it is true if you near interactive object
     {
         float distance = Vector3.Distance(transform.position, hero.transform.position);
@@ -54,3 +60,4 @@ public class AN_DoorKey : MonoBehaviour
         keyCountText.text = "Keys: " + hero.RedKeyCount;
     }
 }
+
